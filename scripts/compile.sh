809f148eebcf04bd4145e5babc259d299b48052c
Thiago M <rpj134@gmail.com>
2023-08-02 11:41:39 -0300

    scripts for fast comands in setup.sh

--- scripts/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/RoboFEI-HT_2023_SOFTWARE
-colcon build --symlink-install
-source install/setup.bash
--- scripts/vision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/RoboFEI-HT_2023_SOFTWARE
-source install/setup.bash
-ros2 run vision_yolov7 detect
