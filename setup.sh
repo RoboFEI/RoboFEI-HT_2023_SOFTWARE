f7eb70315138c57175b9d72373c00c6fefe1def7
Thiago M <rpj134@gmail.com>
2023-08-02 15:52:19 -0300

    ros2 instalation update

--- setup.sh
+++ setup.sh
@@ -15,11 +15,12 @@ username=$(whoami)
 echo "$username ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
 
 echo -e "${blue} Instalação do ROS2...${NC}"
-apt-cache policy | grep universe
-sudo apt update && sudo apt install curl gnupg lsb-release -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update 
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
 sudo apt upgrade -y
 sudo apt install ros-humble-desktop -y
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
@@ -121,5 +122,5 @@ sudo dpkg -i ~/Downloads/forticlient.deb
 sudo apt install -f -y
 
 #Dependence for GameController
-sudo apt  install ant
-sudo apt install openjdk-19-jdk
+sudo apt  install ant -y
+sudo apt install openjdk-19-jdk -y
