672fc7a808e7f38b34c69b5e24ab631f21e6d506
Thiago M <rpj134@gmail.com>
2024-03-07 15:18:39 -0300

    begin to pass neck node to control pkg

---
+++ src/control/include/neck_control/neck_control.hpp
@@ -0,0 +1,51 @@
+#ifndef NECK_CONTROL_HPP_
+#define NECK_CONTROL_HPP_
+
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "custom_interfaces/msg/vision.hpp"
+#include "custom_interfaces/msg/neck_position.hpp"
+
+struct ball_information
+{
+  bool detected   = false;
+  bool left       = false;
+  bool left       = false;
+  bool right      = false;
+  bool right      = false;
+  bool far        = false;
+  bool med        = false;
+  bool close      = false;
+};
+
+
+using namespace std::chrono_literals;
+
+
+class NeckNode : public rclcpp::Node
+{
+  public:
+    using VisionInfo = custom_interfaces::msg::Vision;
+    using NeckPosition = custom_interfaces::msg::NeckPosition;
+    
+    custom_interfaces::msg::Vision::SharedPtr ball_info
+
+
+    NeckNode();
+    virtual ~NeckNode();
+
+  private:
+    rclcpp::Subscription<BallPosition>::SharedPtr vision_subscriber_;
+    rclcpp::Subscription<NeckPosition>::SharedPtr neck_position_subscriber_;
+    rclcpp::Publisher<NeckPosition>::SharedPtr set_neck_position_publisher_;
+    
+};
+
+#endif 
+
---
+++ src/control/src/neck_control.cpp
@@ -0,0 +1,17 @@
+#include "neck_control/neck_control.hpp"
+
+NeckNode::NeckNode()
+: Node("neck_node")
+{
+    RCLCPP_INFO(this->get_logger(), "Run neck node");
+
+    vision_subscriber_ = this->create_subscription<VisionInfo>(
+      "/ball_position", 10, std::bind(&NeckNode::listener_callback_vision, this, std::placeholders::_1));
+    
+
+    neck_position_subscriber_ = this->create_subscription<NeckPosition>(
+      "/neck_position", 10, std::bind(&NeckNode::listener_callback_neck, this, std::placeholders::_1));
+    
+
+    set_neck_position_publisher_ = this->create_publisher<NeckPosition>("/set_neck_position", 10)
+}
