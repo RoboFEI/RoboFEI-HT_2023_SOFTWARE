c941ed371f124e05b397b9760e68772eee61f03b
Thiago M <rpj134@gmail.com>
2024-03-08 21:19:57 -0300

    control launch in robot bringup

--- src/robot_bringup/launch/control_bringup.launch.py
+++ src/robot_bringup/launch/control_bringup.launch.py
@@ -1,4 +1,4 @@
-# $ ros2 launch control action.launch.py
+# $ ros2 launch robot_bringup control_bringup.launch.py
  
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -15,6 +15,13 @@ def generate_launch_description():
         executable="gait_publisher",
         output = 'screen'
     )
+    neck_control = Node(
+        package="control",
+        executable="neck_control",
+        output = 'screen'
+    )
+
     ld.add_action(control)
     ld.add_action(gait)
+    ld.add_action(neck_control)
     return ld
