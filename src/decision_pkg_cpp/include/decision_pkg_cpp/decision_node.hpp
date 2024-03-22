73f491586b4489922e4a74f13a8abd3aab9e4518
Thiago M <rpj134@gmail.com>
2024-03-22 09:22:54 -0300

    imu callback created

--- src/decision_pkg_cpp/include/decision_pkg_cpp/decision_node.hpp
+++ src/decision_pkg_cpp/include/decision_pkg_cpp/decision_node.hpp
@@ -6,29 +6,86 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
 #include "custom_interfaces/msg/humanoid_league_msgs.hpp"
 #include "custom_interfaces/msg/neck_position.hpp"
+#include "custom_interfaces/action/control.hpp"
 
 #include "geometry_msgs/msg/vector3_stamped.hpp"
 #include "sensor_msgs/msg/imu.hpp"
+
+#define FALL_ACCEL_TH 7.0
+
+enum class Move
+{
+    stand_still     = 1,
+    greeting        = 2,
+    right_kick      = 3,
+    left_kick       = 4,
+    turn_right      = 5,
+    turn_left       = 6,
+    goodbye         = 7,
+    walk            = 14,
+    gait            = 15,
+    stand_up_back   = 16,
+    stand_up_front  = 17
+};
+
+enum FallStatus
+{
+    NotFallen,
+    FallenFront,
+    FallenBack,
+    FallenRight,
+    FallenLeft
+};
+
+struct Acceleration
+{
+    float x;
+    float y;
+    float z;
+};
+
+
+struct Robot
+{
+    FallStatus fall = NotFallen;
+    Move movement = Move::stand_still;
+};
+
+
 
 class DecisionNode : public rclcpp::Node
 {
     public:
         using GameControllerMsg = custom_interfaces::msg::HumanoidLeagueMsgs;
         using NeckPosMsg = custom_interfaces::msg::NeckPosition;
+        using ControlActionMsg = custom_interfaces::action::Control;
+        using GoalHandleControl = rclcpp_action::ClientGoalHandle<ControlActionMsg>;
         using ImuGyroMsg = geometry_msgs::msg::Vector3Stamped;
         using ImuAccelMsg = sensor_msgs::msg::Imu;
+        
 
         GameControllerMsg   gc_info;
         NeckPosMsg          neck_pos;
-        ImuGyroMsg             imu_gyro;
+        ImuGyroMsg          imu_gyro;
+        Move                move;
+        Robot               robot;
+
+        int falses_fallen_counter = 0;
+
 
         void listener_callback_GC(const GameControllerMsg::SharedPtr gc_info);
         void listener_callback_neck_pos(const NeckPosMsg::SharedPtr neck_pos);
         void listener_callback_imu_gyro(const ImuGyroMsg::SharedPtr imu_gyro);
+        void listener_callback_imu_accel(const ImuAccelMsg::SharedPtr imu_accel);
+        void send_goal(const Move &move);
 
+        void robot_detect_fallen(const float &robot_accel_x,
+                                 const float &robot_accel_y,
+                                 const float &robot_accel_z);
 
         void main_callback();
 
@@ -40,8 +97,14 @@ class DecisionNode : public rclcpp::Node
         rclcpp::Subscription<GameControllerMsg>::SharedPtr gc_subscriber_;
         rclcpp::Subscription<NeckPosMsg>::SharedPtr neck_position_subscriber_;
         rclcpp::Subscription<ImuGyroMsg>::SharedPtr imu_gyro_subscriber_;
+        rclcpp::Subscription<ImuAccelMsg>::SharedPtr imu_accel_subscriber_;
+
+        rclcpp_action::Client<ControlActionMsg>::SharedPtr action_client_;
 
         rclcpp::TimerBase::SharedPtr main_timer_;
+
+        void goal_response_callback(std::shared_future<GoalHandleControl::SharedPtr> future)
+
 };
 
 
--- src/decision_pkg_cpp/src/decision_node.cpp
+++ src/decision_pkg_cpp/src/decision_node.cpp
@@ -11,19 +11,31 @@ DecisionNode::DecisionNode() : Node("decision_node")
     gc_subscriber_ = this->create_subscription<GameControllerMsg>(
         "gamestate",
         rclcpp::QoS(10),
-        std::bind(&DecisionNode::listener_callback_GC, this, std::placeholders::_1));
+        std::bind(&DecisionNode::listener_callback_GC, this, std::placeholders::_1)
+    );
 
     neck_position_subscriber_ = this->create_subscription<NeckPosMsg>(
         "neck_position",
         rclcpp::QoS(10),
-        std::bind(&DecisionNode::listener_callback_neck_pos, this, std::placeholders::_1));
+        std::bind(&DecisionNode::listener_callback_neck_pos, this, std::placeholders::_1)
+    );
 
     imu_gyro_subscriber_ = this->create_subscription<ImuGyroMsg>(
         "imu/rpy",
         rclcpp::QoS(10),
-        std::bind(&DecisionNode::listener_callback_imu_gyro, this, std::placeholders::_1));
+        std::bind(&DecisionNode::listener_callback_imu_gyro, this, std::placeholders::_1)
+    );
 
+    imu_accel_subscriber_ = this->create_subscription<ImuAccelMsg>(
+        "imu/data",
+        rclcpp::QoS(10),
+        std::bind(&DecisionNode::listener_callback_imu_accel, this, std::placeholders::_1)
+    );
 
+    this->action_client_ = rclcpp_action::create_client<ControlActionMsg>(
+      this,
+      "control_action"
+    );
 
 
 
@@ -55,11 +67,88 @@ void DecisionNode::listener_callback_imu_gyro(const ImuGyroMsg::SharedPtr imu_gy
     //RCLCPP_INFO(this->get_logger(), "Recive Imu Gyro Info");
 }
 
+void DecisionNode::listener_callback_imu_accel(const ImuAccelMsg::SharedPtr imu_accel)
+{
+    robot_detect_fallen( imu_accel->linear_acceleration.x,
+                        -imu_accel->linear_acceleration.y,
+                        -imu_accel->linear_acceleration.z); 
+
+    //RCLCPP_INFO(this->get_logger(), "Recive Imu Accel Info");
+}
+
+void DecisionNode::robot_detect_fallen(const float &robot_accel_x,
+                                       const float &robot_accel_y,
+                                       const float &robot_accel_z)
+{
+    // VERIFICAR O FUNCIONAMENTO ASSIM
+    // if(abs(robot_accel_y) < FALL_ACCEL_TH)
+    // {
+    //     falses_fallen_counter += 1;
+    // }
+    // else falses_fallen_counter = 0;
+
+
+    if( abs(robot.accel.x) > FALL_ACCEL_TH ||
+        abs(robot.accel.z) > FALL_ACCEL_TH)
+    {
+        falses_fallen_counter += 1;
+    }
+    else
+    {
+        falses_fallen_counter = 0;
+        this->robot.fall = NotFallen;
+    } 
+
+    if(falses_fallen_counter > 30)
+    {
+        if(robot_accel_z > FALL_ACCEL_TH)       this->robot.fall = FallenFront;
+        else if(robot_accel_z < -FALL_ACCEL_TH) this->robot.fall = FallenBack;
+        else if(robot_accel_x > FALL_ACCEL_TH)  this->robot.fall = FallenLeft;
+        else this->robot.fall = FallenRight  
+    }
+}
+
+void DecisionNode::send_goal(const Move &move)
+{
+
+    if (!this->action_client_->wait_for_action_server()) {
+      RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+      rclcpp::shutdown();
+    }
+
+    auto goal_msg = ControlActionMsg::Goal();
+    goal_msg.action_number = int(move);
+
+    RCLCPP_INFO(this->get_logger(), "Sending goal %d", goal_msg.action_number);
+
+    auto send_goal_options = rclcpp_action::Client<ControlActionMsg>::SendGoalOptions();
+    send_goal_options.goal_response_callback =
+      std::bind(&DecisionNode::goal_response_callback, this, _1);
+
+    
+    // send_goal_options.feedback_callback =
+    //   std::bind(&FibonacciActionClient::feedback_callback, this, _1, _2);
+    // send_goal_options.result_callback =
+    //   std::bind(&FibonacciActionClient::result_callback, this, _1);
+    // this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+}
+
+void DecisionNode::goal_response_callback(std::shared_future<GoalHandleControl::SharedPtr> future)
+  {
+    auto goal_handle = future.get();
+    if (!goal_handle) {
+      RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+    }
+  }
+
+
 void DecisionNode::main_callback()
 {
-    RCLCPP_INFO(this->get_logger(), "Primary GameState: %d", this->gc_info.game_state);
-    RCLCPP_INFO(this->get_logger(), "id19: %d / id20: %d", this->neck_pos.position19, this->neck_pos.position20); 
-
+    // RCLCPP_INFO(this->get_logger(), "Primary GameState: %d", this->gc_info.game_state);
+    // RCLCPP_INFO(this->get_logger(), "id19: %d / id20: %d", this->neck_pos.position19, this->neck_pos.position20);
+    send_goal(Move::gait);
 }
 
 int main(int argc, char * argv[])
