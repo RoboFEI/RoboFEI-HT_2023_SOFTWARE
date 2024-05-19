ed223b5cfb4b63368a46c6ff9cb39ae5e97f176b
Thiago M <rpj134@gmail.com>
2024-05-19 17:07:03 -0300

    new motor package: open serial port

--- src/new_motors_pkg/src/motors_communication.cpp
+++ src/new_motors_pkg/src/motors_communication.cpp
@@ -1,12 +1,18 @@
 #include "new_motors_pkg/motors_communication.hpp"
 
+// Default setting
+#define BAUDRATE 1000000  // Default Baudrate of DYNAMIXEL X series
+#define DEVICE_NAME "/dev/motors"  // [Linux]: "/dev/ttyUSB", [Windows]: "COM*"
+
+PortHandler *portHandler = PortHandler::getPortHandler(DEVICE_NAME);
+
 MotorsCommunication::MotorsCommunication()
 : Node("read_write_node")
 {
-  RCLCPP_INFO(this->get_logger(), "Run read write node");
+    RCLCPP_INFO(this->get_logger(), "Run read write node");
 
-  timer_ = this->create_wall_timer(
-  8ms, std::bind(&MotorsCommunication::timer_callback, this));
+    timer_ = this->create_wall_timer(
+    8ms, std::bind(&MotorsCommunication::timer_callback, this));
 }
 
 MotorsCommunication::~MotorsCommunication()
@@ -15,17 +21,44 @@ MotorsCommunication::~MotorsCommunication()
 
 void MotorsCommunication::timer_callback()
 {
-  RCLCPP_INFO(this->get_logger(), "node fine");
+  RCLCPP_INFO_ONCE(this->get_logger(), "node fine");
+}
 
+bool openSerialPort()
+{
+    dxl_comm_result = portHandler->openPort();
+    if (dxl_comm_result == false) {
+        RCLCPP_ERROR(rclcpp::get_logger("read_write_node"), "Failed to open the port!");
+        return false;
+    } else {
+        RCLCPP_INFO(rclcpp::get_logger("read_write_node"), "Succeeded to open the port.");
+    }
+
+    // Set the baudrate of the serial port (use DYNAMIXEL Baudrate)
+    dxl_comm_result = portHandler->setBaudRate(BAUDRATE);
+    if (dxl_comm_result == false) {
+        RCLCPP_ERROR(rclcpp::get_logger("read_write_node"), "Failed to set the baudrate!");
+        return false;
+    } else {
+        RCLCPP_INFO(rclcpp::get_logger("read_write_node"), "Succeeded to set the baudrate.");
+    }
+
+    return true;
 }
 
 int main(int argc, char * argv[])
 { 
-  rclcpp::init(argc, argv);
+    char string1[50]; //String
+    sprintf(string1,"echo 123456| sudo -S renice -20 -p %d", getpid());
+    system(string1);//prioridade
 
-  auto motorscomm = std::make_shared<MotorsCommunication>();
-  rclcpp::spin(motorscomm);
-  rclcpp::shutdown();
+    if(!openSerialPort()) return -1;
 
-  return 0;
+    rclcpp::init(argc, argv);
+
+    auto motorscomm = std::make_shared<MotorsCommunication>();
+    rclcpp::spin(motorscomm);
+    rclcpp::shutdown();
+
+    return 0;
 }
