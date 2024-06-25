055fdf5b50a495ce078117195c4d204e21d125cd
Thiago M <rpj134@gmail.com>
2024-06-25 14:57:33 -0300

    nda

--- src/motors_pkg/include/motors_pkg/motors_communication.hpp
+++ src/motors_pkg/include/motors_pkg/motors_communication.hpp
@@ -2,13 +2,10 @@
 #define MOTORS_COMMUNICATION_HPP_
 
 #include <chrono>
-#include <chrono>
-#include <chrono>
 #include <memory>
 #include <vector>
 #include <numeric>
 #include <unistd.h>
-#include <numeric>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
@@ -29,39 +26,6 @@ typedef struct Joints
 }Joints;
 
 
-class MotorsCommunication : public rclcpp::Node
-
-#include "custom_interfaces/msg/joint_state.hpp"
-
-
-using namespace dynamixel;
-using namespace std::chrono_literals;
-using std::placeholders::_1;
-
-typedef struct Joints
-{
-  std::vector<std::uint32_t> position = std::vector<std::uint32_t>(21, 2048);
-  std::vector<std::uint32_t> velocity = std::vector<std::uint32_t>(21, 0);
-  std::vector<std::uint8_t> torque = std::vector<std::uint8_t>(21, 1);
-}Joints;
-
-
-class MotorsCommunication : public rclcpp::Node
-
-#include "custom_interfaces/msg/joint_state.hpp"
-
-
-using namespace dynamixel;
-using namespace std::chrono_literals;
-using std::placeholders::_1;
-
-typedef struct Joints
-{
-  std::vector<std::uint32_t> position = std::vector<std::uint32_t>(21, 2048);
-  std::vector<std::uint32_t> velocity = std::vector<std::uint32_t>(21, 0);
-  std::vector<std::uint8_t> torque = std::vector<std::uint8_t>(21, 1);
-}Joints;
-
 
 class MotorsCommunication : public rclcpp::Node
 {
@@ -77,64 +41,8 @@ public:
 
   int dxl_comm_result = COMM_TX_FAIL;
 
-
-  
-  void joint_state_callback(const JointStateMsg::SharedPtr joint_state_info);
-  using JointStateMsg = custom_interfaces::msg::JointState;
-
-  PacketHandler  *packetHandler;
-  GroupSyncWrite *groupSyncWritePos;
-  GroupSyncRead  *groupSyncReadPos;
-  
-  Joints joints;
-  std::vector<int> allIds = std::vector<int>(20);
-
-  int dxl_comm_result = COMM_TX_FAIL;
-
-
-  
-  void joint_state_callback(const JointStateMsg::SharedPtr joint_state_info);
-  using JointStateMsg = custom_interfaces::msg::JointState;
-
-  PacketHandler  *packetHandler;
-  GroupSyncWrite *groupSyncWritePos;
-  GroupSyncRead  *groupSyncReadPos;
-  
-  Joints joints;
-  std::vector<int> allIds = std::vector<int>(20);
-
-  int dxl_comm_result = COMM_TX_FAIL;
-
-
-  
   void joint_state_callback(const JointStateMsg::SharedPtr joint_state_info);
   void timer_callback();
-
-  void initialMotorsSetup(int id);
-  void setJoints(JointStateMsg jointInfo);
-  void setJointVel(int id, int goalVel);
-  void setJointTorque(int id, int goalTorque);
-  void setAllJointPos();
-  void getNoTorquePos();
-
-
-  uint8_t* convertInfo(int jointInfo);
-
-  MotorsCommunication();
-  virtual ~MotorsCommunication();
-
-  void initialMotorsSetup(int id);
-  void setJoints(JointStateMsg jointInfo);
-  void setJointVel(int id, int goalVel);
-  void setJointTorque(int id, int goalTorque);
-  void setAllJointPos();
-  void getNoTorquePos();
-
-
-  uint8_t* convertInfo(int jointInfo);
-
-  MotorsCommunication();
-  virtual ~MotorsCommunication();
 
   void initialMotorsSetup(int id);
   void setJoints(JointStateMsg jointInfo);
@@ -153,19 +61,7 @@ private:
   rclcpp::Subscription<JointStateMsg>::SharedPtr joint_state_subscription_;
   rclcpp::Publisher<JointStateMsg>::SharedPtr all_joints_position_publisher;
 
-  rclcpp::Subscription<JointStateMsg>::SharedPtr joint_state_subscription_;
-  rclcpp::Publisher<JointStateMsg>::SharedPtr all_joints_position_publisher;
-
-  rclcpp::Subscription<JointStateMsg>::SharedPtr joint_state_subscription_;
-  rclcpp::Publisher<JointStateMsg>::SharedPtr all_joints_position_publisher;
-
-
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  
+  rclcpp::TimerBase::SharedPtr timer_;  
 };
 
-#endif  // MOTORS_COMMUNICATION_HPP_
-#endif  // MOTORS_COMMUNICATION_HPP_
 #endif  // MOTORS_COMMUNICATION_HPP_
